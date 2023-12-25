3b3ec4fd527cfb602fe706db42f39bb9cf1d3b30
zsj <2951037717@qq.com>
2023-12-25 23:39:57 +0800

    delete basic

--- Ex/Basic.lean
+++
@@ -1,8 +0,0 @@
-theorem thm₁ (a b c d e : Prop) (h : a ↔ b) : (c ∧ a ∧ d → e) ↔ (c ∧ b ∧ d → e) :=
-  propext h ▸ Iff.refl _
-
-theorem thm₂ (a b : Prop) (p : Prop → Prop) (h : a ↔ b) (h₁ : p a) : p b :=
-  propext h ▸ h₁
-
-
---hello
